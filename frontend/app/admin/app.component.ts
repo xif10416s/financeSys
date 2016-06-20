fcc8e56935ff1cb99e653feaa7f9b76e90589662
fxi@huadongmedia.com <12345678>
2016-06-20 20:16:50 +0800

    11111

--- frontend/app/admin/app.component.ts
+++ frontend/app/admin/app.component.ts
@@ -1,11 +1,17 @@
 import { Component ,provide,Inject,OpaqueToken}       from '@angular/core';
-
+import {MdSidenav,MdSidenavLayout} from '@angular2-material/sidenav';
+import {MdCheckbox} from '@angular2-material/checkbox';
+import {MdButton} from '@angular2-material/button';
 
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1>',
-    styleUrls: ['app/style/app.component.css']
+    templateUrl: 'app/admin/app.component.html',
+    styleUrls: ['app/style/app.component.admin.css'],
+    directives: [ROUTER_DIRECTIVES,MdSidenavLayout,MdSidenav,MdCheckbox,MdButton],
+    providers: [
+        ROUTER_PROVIDERS
+    ]
 })
 
 @RouteConfig([
